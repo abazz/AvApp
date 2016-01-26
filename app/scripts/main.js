93469c4dd48fe0cdfab48c9cfb66dcfdf35e30cf
Antti Koivumäki <Antti@MacBook-Pro-3.local>
2016-01-26 15:08:21 +0200

    ready

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,9 @@
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+$(document).ready(function () {
+    $('#checkit').click(function () {
+        alert('It\'s bananas!');
+    });
+    $('#signup').click(function () {
+        alert('Signed');
+    });
+});
